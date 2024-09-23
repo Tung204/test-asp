3854afd6ade595e9c8663c08f0142ec3f7d952ab
Tung204 <nst874@gmail.com>
2024-09-24 01:40:55 +0700

    Update

---
+++ ProjectA/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProjectA.Models;
+
+namespace ProjectA.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+		public DbSet<SanPham> SanPham {  get; set; }
+		public	DbSet<TheLoai> TheLoai { get; set; }
+	}
+}
